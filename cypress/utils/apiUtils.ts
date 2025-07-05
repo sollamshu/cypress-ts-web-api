ba8c8264f4013b28fc672eb1e4b7b1cd9e8ac40c
Sol Lam <sol.lam@gmail.com>
2025-07-04 18:46:36 -0600

    Adjust env variable

--- cypress/utils/apiUtils.ts
+++ cypress/utils/apiUtils.ts
@@ -1,12 +1,12 @@
 export class ApiUtils {
   static get(endpoint: string) {
-    return cy.request('GET', `${Cypress.env('REQRES_API_URL')}/${endpoint}`);
+    return cy.request('GET', `${Cypress.env('API_BASE_URL')}/${endpoint}`);
   }
 
   static post(endpoint: string, body: object) {
     return cy.request(
       'POST',
-      `${Cypress.env('REQRES_API_URL')}/${endpoint}`,
+      `${Cypress.env('API_BASE_URL')}/${endpoint}`,
       body
     );
   }
